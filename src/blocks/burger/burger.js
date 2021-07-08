d0f05fb1b18d595f1b1e3b3178e5f2571baacc70
des-yogi <nuevo-design@mail.ru>
2021-07-08 20:19:42 +0300

    Моб меню в процессе

--- src/blocks/burger/burger.js
+++ src/blocks/burger/burger.js
@@ -2,13 +2,29 @@
 
   // Добавление/удаление модификаторов при клике на переключение видимости
   var toggler = document.getElementById('main-nav-toggler');
+  var body = document.querySelector('body');
+  var mobMenu = document.getElementById('mobile-menu');
+
   if(toggler){
     toggler.addEventListener('click', mainNavVisibleToggle);
 
     function mainNavVisibleToggle(e) {
       e.preventDefault();
       toggler.classList.toggle('burger--close'); // модификатор иконки (должен быть .burger)
-      //document.getElementById('main-nav').classList.toggle('main-nav--open');
+      body.classList.toggle('noscroll');
+      mobMenu.classList.toggle('mobile-menu--open');
+      if (mobMenu.classList.contains('mobile-menu--open')) {
+        console.log('1');
+        mobMenu.removeAttribute('aria-hidden');
+        mobMenu.focus();
+        mobMenu.removeAttribute('tabindex');
+      } else {
+        mobMenu.setAttribute('aria-hidden', true);
+        mobMenu.setAttribute('tabindex', '-1');
+        mobMenu.blur();
+        console.log('2');
+        return;
+      }
     }
   }
 
